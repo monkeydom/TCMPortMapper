b5b706783683951d00a2f07991a038a63f85529c
Dominik Wagner <wagnr@icloud.com>
2008-04-15 11:46:50 +0000

    - initial checkin of 1.0 state

---
+++ portmap/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//
+//  Copyright (c) 2007-2008 TheCodingMonkeys: <http://codingmonkeys.de>
+//  Some rights reserved: <http://opensource.org/licenses/mit-license.php> 
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
