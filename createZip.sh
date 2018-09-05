d872bed3f4007cf2c882e3543e393509a88833dc
Dominik Wagner <wagnr@icloud.com>
2018-09-05 15:52:55 +0200

    Updating the scripts a little for deployment and adding version numbers.

--- createZip.sh
+++ createZip.sh
@@ -1,14 +1,11 @@
 zipBasePath="{TARGET_BUILD_DIR}/${ZipName}"
+git=$(sh /etc/profile; which git)
 
 echo "Removing zip file ${zipBasePath}.zip"
 rm -f "${zipBasePath}.zip"
 if [ x${ACTION} = xclean ]; then
 	exit 0
 fi
-
-echo "Removing .svn directories..."
-find "${BUILT_PRODUCTS_DIR}/${ZipProduct}" -name ".svn" -exec rm -rvf "{}" \;
-echo "...done"
 
 echo "Setting ownership..."
 chgrp -R admin "${BUILT_PRODUCTS_DIR}/${ZipProduct}"
@@ -24,7 +21,9 @@ zip -9 -r -y "${ZipName}.zip" "${ZipProduct}"
 echo "...done"
 
 echo "Adding Revision Number to Zip file ${zipBasePath}.zip ..."
-REV=`/usr/bin/env svnversion -n "${SRCROOT}"`
+cd "${SRCROOT}"
+REV=$("$git" rev-list --first-parent --count HEAD)
+# REV=$("$git" describe --tag --always)
 cd "${BUILT_PRODUCTS_DIR}"
 cp "${ZipName}.zip" "${ZipName}-${REV}.zip"
 echo "...done"
---
+++ set_build_number.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git=$(sh /etc/profile; which git)
+number_of_commits=$("$git" rev-list --first-parent --count HEAD)
+git_release_version=$("$git" describe --tags --always)
+
+target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
+dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
+
+for plist in "$target_plist" "$dsym_plist"; do
+  if [ -f "$plist" ]; then
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
+  fi
+done
