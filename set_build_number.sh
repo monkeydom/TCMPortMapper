5d91b410258a06a9b82ce95c0f908f833bb6d951
Dominik Wagner <wagnr@icloud.com>
2018-09-26 17:09:10 +0200

    Fixed version number setting for the PortMapFramework build (did overwrite the bundle version, instead of write to the private additional BuildRevision keu).

--- set_build_number.sh
+++ set_build_number.sh
@@ -7,8 +7,19 @@ git_release_version=$("$git" describe --tags --always)
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
+
+
+private_revision_key="CFBundleVersion"
+
+if [ "$1" != "" ]; then
+	private_revision_key=$1
+# hack to not try to write a custom key to a plist that doesn't have it
+	dsym_plist=target_plist
+fi 
+
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
+	echo "/usr/libexec/PlistBuddy -c \"Set :$private_revision_key $number_of_commits\"" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :$private_revision_key $number_of_commits" "$plist"
   fi
 done
